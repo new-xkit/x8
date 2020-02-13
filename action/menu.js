befe277d56343afe15b8393d68cde8ac92224558
April Sylph <april@sylph.tech>
2020-02-13 04:37:01 +0000

    add popup to allow configuration of enabled scripts

---
+++ action/menu.js
@@ -0,0 +1,36 @@
+const installedScripts = [
+  {name: 'reblog_timestamps', title: 'Reblog Timestamps'},
+  {name: 'all_features', title: 'All Features'},
+];
+
+const scriptsList = document.getElementById('scriptslist');
+const writeEnabled = () => {
+  const enabledScripts = [];
+
+  for (const listItem of scriptsList.children) {
+    const input = listItem.getElementsByTagName('input')[0];
+    if (input.checked === true) {
+      enabledScripts.push(input.id);
+    }
+  }
+
+  browser.storage.local.set(
+    {enabledScripts},
+  ).catch(console.error);
+};
+
+installedScripts.forEach(script => {
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `
+    <input type="checkbox" id="${script.name}">
+    <label for="${script.name}">${script.title}</label>
+  `;
+
+  scriptsList.appendChild(listItem);
+  document.getElementById(script.name).oninput = writeEnabled;
+});
+
+browser.storage.local.get('enabledScripts').then(results => {
+  const {enabledScripts} = results;
+  enabledScripts.forEach(scriptName => document.getElementById(scriptName).checked = true);
+});
--- inject.js
+++ inject.js
@@ -1,10 +1,14 @@
 const {getURL} = browser.runtime;
 
-const installedScripts = ['reblog_timestamps', 'all_features'];
-installedScripts.forEach(name => {
-  const script = document.createElement('script');
-  script.type = 'module';
-  script.src = getURL(`src/${name}.js`);
-  document.documentElement.appendChild(script);
-  script.parentNode.removeChild(script);
+browser.storage.local.get('enabledScripts').then(results => {
+  const {enabledScripts} = results;
+
+  enabledScripts.forEach(name => {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = getURL(`src/${name}.js`);
+
+    document.documentElement.appendChild(script);
+    script.parentNode.removeChild(script);
+  });
 });
