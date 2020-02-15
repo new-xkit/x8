66473dbbceeb7d949c5fd6fe0e5d8993a843549a
nightpool <eg1290@gmail.com>
2020-02-15 13:21:31 -0500

    Only run on redpop pages

--- inject.js
+++ inject.js
@@ -1,10 +1,13 @@
 const {getURL} = browser.runtime;
+const redpop = [...document.scripts].filter(i => i.src.match('/pop/')).length;
 
-const installedScripts = ['reblog_timestamps', 'all_features'];
-installedScripts.forEach(name => {
-  const script = document.createElement('script');
-  script.type = 'module';
-  script.src = getURL(`src/${name}.js`);
-  document.documentElement.appendChild(script);
-  script.parentNode.removeChild(script);
-});
+if (redpop) {
+  const installedScripts = ['reblog_timestamps', 'all_features'];
+  installedScripts.forEach(name => {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = getURL(`src/${name}.js`);
+    document.documentElement.appendChild(script);
+    script.parentNode.removeChild(script);
+  });
+}
